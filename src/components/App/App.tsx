d73d69d66951cd4a66f4662b4be36c90e8b38c34
Dylan <klj40702@gmail.com>
2021-02-27 16:52:14 +0800

    Use esbuild

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import BankContent from '../BankContent';
 
 function App() {
--- src/components/BankContent/ProcessItem/ProcessItem.tsx
+++ src/components/BankContent/ProcessItem/ProcessItem.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 
 import { QueueContent } from '../../../types/misc';
 import { queueSubject$ } from '../../../utils/bankHelpers';
